dbbdc7fac31065dc7e727af7249f81b1e5292fb4
tshdi60@gmail.com <tshdi60@gmail.com>
2022-08-04 14:43:00 +0600

    Commit from phone

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 // 1st function 
-
+/* 
 function bringSingara(money, quentity){
     let money1 = money;
     let quentity1 = quentity;
@@ -13,3 +13,34 @@ function bringSingara(money, quentity){
 let singara = bringSingara(200, 15);
 
 console.log(singara)
+
+*/
+
+let myDetils = {
+  name: 'Tahdi Islam', 
+  age: 20, 
+  height:'5.7 Inch',
+};
+
+//let myName = myDetils.name;
+//console.log(myName)
+
+//let myName = myDetils['name']
+//console.log(myName)
+
+// let myName1 = 'name';
+// let myName = myDetils[myName1];
+// console.log(myName)
+
+
+let keys = Object.keys(myDetils);
+//console.log(keys);
+
+let values = Object.values(myDetils);
+//console.log(values);
+
+for(i = 0; i < keys.length; i++){
+  let key = keys[i];
+  let value = values[key];
+  console.log(key, value);
+}
