12ec9e0ac2124244298a742697296a30cac1e909
tahdi-islam <tshdi60@gmail.com>
2022-08-05 00:32:26 +0600

    solve problem 3

--- problem_solving.js
+++ problem_solving.js
@@ -28,3 +28,24 @@ let average2 = average.toFixed(2);
 let average3 = parseFloat(average2)
 console.log(typeof average3)
 */
+
+// 3rd problem
+/* 
+function make_avg(array, arrayLength){
+  let arrayLength2 = arrayLength
+  var sum = 0;
+  for (let i = 0; i < arrayLength; i++){
+    sum += array[i];
+  }
+  let average = sum / arrayLength2;
+  let average2 = average.toFixed(2);
+  let average3 = parseFloat(average2)
+  return average3;
+}
+
+
+let array3 = [45, 64, 34, 64, 64, 22, 64, 67, 64, 35, 62];
+let arrayLength3 = array3.length;
+let make_averege = make_avg(array3, arrayLength3);
+console.log(make_averege)
+ */
