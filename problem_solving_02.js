50578eff46244f8f776c9eabc5693647c3db28dc
tahdi-islam <tshdi60@gmail.com>
2022-08-05 21:53:13 +0600

    solve prb_1

---
+++ problem_solving_02.js
@@ -0,0 +1,15 @@
+// 1st problem
+/* 
+function convertHourToMinutes(hour){
+	const minutes = hour * 60;
+	return minutes;
+}
+
+const hour = 4;
+
+const minutes = convertHourToMinutes(hour);
+
+console.log(hour, ' Hour = ', minutes, 'minutes')
+
+ */
+
