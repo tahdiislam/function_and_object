abe3f3c3cbd066f7f8eab4898a889fadc19328ec
tahdi-islam <tshdi60@gmail.com>
2022-08-06 03:36:57 +0600

    solve present problem

---
+++ problem_solving_03.js
@@ -0,0 +1,25 @@
+// 1st problem
+
+// function celToFahren(celsius) {
+// 	let fahrenhiet = (celsius * (9/5)) + 32;
+// 	return fahrenhiet;
+// }
+
+// const celsicus = 32;
+
+// const fahrenhiet = celToFahren(celsicus);
+
+// console.log(fahrenhiet)
+
+// 2nd problem 
+/* 
+function fahrenToCel(fahrenhiet){
+    const celsicus = (fahrenhiet - 32) * (5/9);
+    return celsicus;
+}
+
+const fahrenhiet = 90;
+
+const celsicus = fahrenToCel(fahrenhiet);
+
+console.log(celsicus) */
