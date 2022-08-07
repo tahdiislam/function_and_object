84e5f79415a28c1ba2c1365f0f56056bd67a0970
tahdi-islam <tshdi60@gmail.com>
2022-08-07 23:10:33 +0600

    practice

---
+++ practiceOfModule21.js
@@ -0,0 +1,18 @@
+/*  const myAddress = "222/A, Shamoli Tower, Shamoli";
+// console.log(myAddress.length)
+
+const searchElement = "shamoli";
+
+const searchElementMakeLowerCase = searchElement.toLowerCase();
+
+const makeArrayToLowerCase = myAddress.toLowerCase();
+
+const findTheElement = makeArrayToLowerCase.includes(searchElementMakeLowerCase);
+// console.log(findTheElement); 
+
+// one line includes serarch 
+
+const findAElementFromString = myAddress.toLowerCase().includes(searchElement.toLowerCase());
+
+console.log(findAElementFromString) */
+
