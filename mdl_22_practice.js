0912a0c597357a3b2b6db2ed8cfe82d548f50b12
tahdi-islam <tshdi60@gmail.com>
2022-08-08 12:11:08 +0600

    solve some problem

--- mdl_22_practice.js
+++ mdl_22_practice.js
@@ -1,3 +1,40 @@
-const array1 = [3434, 3534, 4553421, 34324, 532423, 5234324, 324234, 234234, 2353425, 5234451, 213424, 5424];
+/* const array1 = [3434, 3534, 4553421, 34324, 532423, 5234324, 324234, 234234, 2353425, 5234451, 213424, 5424];
 
 console.log(Math.min(...array1));
+
+console.log(Math.round(Math.random()*1000000000000000)) */
+
+// 2nd problem
+/* 
+const num1 = 3434;
+const num2 = 33;
+const num3 = 33;
+
+console.log(Math.min(num1, num2, num3)) */
+
+// 3rd problem
+/* 
+function average(numbers){
+  let sum = 0; 
+  for(let i =0; i < numbers.length; i ++){
+    sum += numbers[i];
+  }
+  let aver = sum / numbers.length;
+  return aver;
+}
+
+const numbersArray = [73,82,81,53]
+let findAver = average(numbersArray);
+console.log(findAver);
+*/
+/* 
+function volumeOfRectangle(length, width, height){
+  const volume = length * width * height;
+  return volume;
+}
+const length2 = 5;
+const width2 = 6;
+const height2 = 8;
+const volume = volumeOfRectangle(length2, width2, height2);
+console.log(volume);
+*/
