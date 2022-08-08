91f6caca2b37222b60f21d98549fca600c142181
tahdi-islam <tshdi60@gmail.com>
2022-08-09 02:47:46 +0600

    foobar

--- mld_22.js
+++ mld_22.js
@@ -27,3 +27,21 @@ function rmDublicates(){
 const uniqueName = rmDublicates(names);
 console.log(uniqueName)
  */
+
+// fizz buzz
+/* 
+for (let i = 0; i <= 50; i++){
+  if(i % 15 === 0){
+    console.log('foobar')
+  }
+  else if (i % 3 === 0){
+    console.log("foo")
+  }
+   else if (i % 5 === 0){
+    console.log('bar')
+  } 
+   else{
+    console.log(i)
+  }
+} 
+*/
