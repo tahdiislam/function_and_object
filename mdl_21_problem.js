b6344db503b1704da0331ea9e86edb097dd5ea21
tahdi-islam <tshdi60@gmail.com>
2022-08-09 00:37:04 +0600

    practice the module 21

--- mdl_21_problem.js
+++ mdl_21_problem.js
@@ -135,3 +135,22 @@ dela = 89;
 const getTheCake2 = Math.min(bela, chinku, dela);
 
 console.log(getTheCake2) */
+
+// fibor moto nacho..
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34......
+
+/* 
+  fibo[3] = fibo[1] + fibo[2]
+  fibo[4] = fibo[3] + fibo[2]
+  fibo[5] = fibo[4] + fibo[3]
+  fibo[6] = fibo[5] + fibo[4]
+  fibo[7] = fibo[6] + fibo[5]
+  fibo[n] = fibo[n-1] + fibo[n-2]
+*/
+
+// const fibo = [0, 1];
+// for(let i = 2; i <= 10; i++){
+//   fibo[i] = fibo[i - 1] + fibo[ i - 2];
+// }
+// console.log(fibo)
