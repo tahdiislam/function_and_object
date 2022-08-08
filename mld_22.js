d0bf9ce4f483c062c136f808e4010d906651ea14
tahdi-islam <tshdi60@gmail.com>
2022-08-09 02:16:30 +0600

    solve a problem

---
+++ mld_22.js
@@ -0,0 +1,29 @@
+// Array.slice() & Array.splice()
+
+/* const numbers = [343, 345, 563, 324, 232];
+
+// const numberSlice = numbers.slice(1, 4);
+// console.log(numberSlice);
+
+const numberSplice = numbers.splice(0, 2, 2, 533, 34, 343, 534, 642, 342, 53432);
+
+console.log(numberSplice)
+console.log(numbers) */
+
+// remove the duplicates
+/* 
+const names = ['abul', 'babul', 'cabul', 'kabul', 'babul', 'cabul', 'kabul', 'bulbul', 'culbul', 'bulbul']
+function rmDublicates(){
+  const unique = [];
+  for (let i = 0; i < names.length; i++){
+    const name = names[i];
+    if(unique.includes(name) === false){
+      unique.push(name);
+    }
+  }
+  return unique;
+}
+
+const uniqueName = rmDublicates(names);
+console.log(uniqueName)
+ */
